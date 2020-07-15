5ff268d3cffb0f2d88cd1d73d39afc8a6a5d7178
AL00448 <hagirin@yamaha-motor.co.jp>
2020-07-15 14:20:15 +0900

    second commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const msalConfig = {
   auth: {
-    clientId: '4efab3ea-2033-4930-a378-9837418e8e2b',
-    redirectUri: 'http://localhost:8080'
+    clientId: '2aa23749-177c-4650-be79-a7f92385eac1',
+    redirectUri: 'https://hagiri.github.io/office365ymslchatbot/'
   },
   cache: {
     cacheLocation: "sessionStorage",
